be05ee4ee56a1551d0e74b1a8d991b954e4286df
der-rozanov <der-rozanov@yandex.ru>
2022-06-14 13:45:58 +0300

    Alpha 1.1

--- COM_Reader/main.cpp
+++ COM_Reader/main.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
+    //example 
     ComReader Ard(L"COM3", 9600, 8);
     std::vector<char> inp_str;
 
+    while (1)
+    {
+        inp_str = Ard.getCharData();
+            for (int i = 0; i < Ard.getSizeData(); i++)
+            {
+                std::cout << inp_str[i];
+            }
+            std::cout << std::endl;
+    }
+    //example
 
     return 0;
 }
